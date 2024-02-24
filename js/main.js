2ba58c05eb66078a9594e44c34ec8ac89da25932
emmanuel okezie <emmanuelokezie50@gmail.com>
2024-02-24 13:47:49 +0100

    adding function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 let items = document.querySelectorAll('.item')
 
 
+function calculatetotal() {
+    let subtotal =document.querySelector('.Subtotal')
+    let tax =document.querySelector('.vats')
+    let grandtotal =document.querySelector('.gtotal')
+    let sub=0
+    items.forEach(item => {
+        const total =item.querySelector('.total')
+        sub+=parseFloat(total.innerHTML)
+    }) 
+    subtotal.innerHTML = `$${sub}`
+    grandtotal.innerHTML = `$${sub+ parseFloat(vats.innerHTML)}`
+}
+
+
+
+
 items.forEach(item => {
    
     const amut = parseFloat(item.querySelector('.amut').innerHTML)
